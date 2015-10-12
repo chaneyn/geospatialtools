3cf644e9b8725214d44453e38ee33e7c2e2764a5
Nathaniel W. Chaney <nchaney@aos-c02q32.princeton.edu>
2015-10-11 20:48:58 -0400

    Fixed length error

--- src/terrain_tools.f90
+++ src/terrain_tools.f90
@@ -115,7 +115,7 @@ subroutine calculate_mfd_acc(dem,res,area,nx,ny,p)
  catchment(:,:) = 0
  do i=1,nx
   do j=1,ny
-   call neighbr_check_mfd(i,j,dem,catchment,positions,nx,ny,npos,p)
+   call neighbr_check_mfd(i,j,dem,catchment,positions,nx,ny,npos,p,res)
   enddo
  enddo
 
@@ -124,11 +124,11 @@ subroutine calculate_mfd_acc(dem,res,area,nx,ny,p)
 
 end subroutine
 
-recursive subroutine neighbr_check_mfd(i,j,dem,catchment,positions,nx,ny,npos,p)
+recursive subroutine neighbr_check_mfd(i,j,dem,catchment,positions,nx,ny,npos,p,res)
  
  implicit none
  integer,intent(in) :: i,j,npos,nx,ny
- real,intent(in) :: p
+ real,intent(in) :: p,res
  real,intent(in),dimension(nx,ny) :: dem
  real,intent(inout),dimension(nx,ny) :: catchment
  integer,intent(in),dimension(npos,2) :: positions
@@ -142,8 +142,8 @@ recursive subroutine neighbr_check_mfd(i,j,dem,catchment,positions,nx,ny,npos,p)
    jnew = j+positions(ipos,2)
    if ((inew .lt. 1) .or. (jnew .lt. 1) .or. (inew .gt. nx) .or. (jnew .gt. ny)) cycle
    if (dem(inew,jnew) .gt. dem(i,j))then
-    call neighbr_check_mfd(inew,jnew,dem,catchment,positions,nx,ny,npos,p)
-    call fract_flow_mfd(i,j,inew,jnew,dem,fract,p,positions,nx,ny,npos)
+    call neighbr_check_mfd(inew,jnew,dem,catchment,positions,nx,ny,npos,p,res)
+    call fract_flow_mfd(i,j,inew,jnew,dem,fract,p,positions,nx,ny,npos,res)
     catchment(i,j) = catchment(i,j) + fract*catchment(inew,jnew)
    endif
   enddo
@@ -151,32 +151,55 @@ recursive subroutine neighbr_check_mfd(i,j,dem,catchment,positions,nx,ny,npos,p)
     
 end subroutine
 
-subroutine fract_flow_mfd(iorg,jorg,i,j,dem,fract,p,positions,nx,ny,npos)
+subroutine fract_flow_mfd(iorg,jorg,i,j,dem,fract,p,positions,nx,ny,npos,res)
 
  implicit none
  integer,intent(in) :: iorg,jorg,i,j,nx,ny,npos
- real,intent(in) :: p
+ real,intent(in) :: p,res
  real,intent(out) :: fract
  real,intent(in),dimension(nx,ny) :: dem
  integer,intent(in),dimension(npos,2) :: positions
- integer :: ipos,inew,jnew
- real :: angle_sum,slope
+ integer :: ipos,inew,jnew,k,l
+ real :: angle_sum,slope,length,slopes(npos)
  slope = 0.0
- angle_sum = 0.0
+ slopes = 0.0
+ !Calculate all the slopes for the surrounding cells
  do ipos=1,npos
   inew = i+positions(ipos,1)
   jnew = j+positions(ipos,2)
   if ((inew .lt. 1) .or. (jnew .lt. 1) .or. (inew .gt. nx) .or. (jnew .gt. ny))cycle
   if (dem(i,j) .gt. dem(inew,jnew))then
-   slope = (dem(i,j) - dem(inew,jnew))/30.0 !HACK
-   angle_sum = angle_sum + slope
+   k = inew - i
+   l = jnew - j
+   if ((k + l .eq. -2) .or. (k + l .eq. 2) .or. (k + l .eq. 0))then
+    length = 1.41421356237*res
+   else
+    length = res
+   endif
+   slopes(ipos) = (dem(i,j) - dem(inew,jnew))/length
   endif
  enddo
+ !Calculate sum of angles
+ angle_sum = sum(slopes)
+ !Calculate fraction
+ k = iorg - i
+ l = jorg - j
+ if ((k + l .eq. -2) .or. (k + l .eq. 2) .or. (k + l .eq. 0))then
+  length = 1.41421356237*res
+ else
+  length = res
+ endif
  if (angle_sum .eq. 0.0)then
   fract = 0.0
  else
-  slope = (dem(i,j) - dem(iorg,jorg))/30.0 !HACK
+  slope = (dem(i,j) - dem(iorg,jorg))/length
   fract = slope/angle_sum
  endif
+
+end subroutine
+
+subroutine calculate_hillslopes()
+
+ !Take the channel network and recurse
 
 end subroutine
